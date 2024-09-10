4f66047893d0f491d8fe18abfcf069eaa1e37fe7
Michał Walczak <michal.walczak@blockydevs.com>
2024-09-10 13:19:55 +0200

    Add execution worker fixtures and scheduler utilities

--- packages/tse-backend/src/modules/strategy-execution/execution-worker.service.ts
+++ packages/tse-backend/src/modules/strategy-execution/execution-worker.service.ts
@@ -1,28 +1,66 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ArbitrageStrategy } from '../trading-strategy/strategies/arbitrage/arbitrage.strategy';
 import { MarketMakingStrategy } from '../trading-strategy/strategies/market-making/market-making.strategy';
-import { Cron, CronExpression } from '@nestjs/schedule';
+import { CronExpression, SchedulerRegistry } from '@nestjs/schedule';
 import { ArbitrageService } from '../trading-strategy/strategies/arbitrage/arbitrage.service';
 import { MarketMakingService } from '../trading-strategy/strategies/market-making/market-making.service';
+import { ConfigService } from '@nestjs/config';
+import { SchedulerUtil } from '../../common/utils/scheduler.utils';
 
 @Injectable()
 export class ExecutionWorkerService {
   private logger = new Logger(ExecutionWorkerService.name);
+  private isJobRunning: boolean = false;
 
   constructor(
     private readonly arbitrageService: ArbitrageService,
     private readonly arbitrageStrategy: ArbitrageStrategy,
     private readonly marketMakingService: MarketMakingService,
-    private readonly marketMakingStrategy: MarketMakingStrategy
-  ) {}
+    private readonly marketMakingStrategy: MarketMakingStrategy,
+    private readonly configService: ConfigService,
+    private readonly schedulerRegistry: SchedulerRegistry,
+    private readonly schedulerUtils: SchedulerUtil
+  ) {
+  }
 
-  @Cron(CronExpression.EVERY_30_SECONDS)
-  async handleCron() {
+  onModuleInit() {
+    this.schedulerUtils.addCronJob(
+      ExecutionWorkerService.name,
+      this.configService.get<string>('CRON_EXPRESSION', CronExpression.EVERY_30_SECONDS),
+      this.handleCron.bind(this),
+      this.schedulerRegistry,
+    )
+  }
+
+  private async processData() {
     this.logger.debug('Strategy Execution Worker started');
-    const arbitrageStrategies = await this.arbitrageService.findRunningStrategies();
-    await this.arbitrageStrategy.start(arbitrageStrategies);
+    try {
+      const arbitrageStrategies = await this.arbitrageService.findRunningStrategies();
+      await this.arbitrageStrategy.start(arbitrageStrategies);
+    } catch (error) {
+      this.logger.error('Error executing arbitrage strategies', error.stack);
+    }
 
-    const marketMakingStrategies = await this.marketMakingService.findRunningStrategies();
-    await this.marketMakingStrategy.start(marketMakingStrategies);
+    try {
+      const marketMakingStrategies = await this.marketMakingService.findRunningStrategies();
+      await this.marketMakingStrategy.start(marketMakingStrategies);
+    } catch (error) {
+      this.logger.error('Error executing market making strategies', error.stack);
+    }
+  }
+
+  async handleCron() {
+    if (this.isJobRunning) {
+      this.logger.warn('Job still running, skipping');
+      return;
+    }
+    this.isJobRunning = true;
+    try {
+      await this.processData();
+    } catch (error) {
+      this.logger.error('Error processing data', error.stack);
+    } finally {
+      this.isJobRunning = false;
+    }
   }
 }
--- packages/tse-backend/src/modules/strategy-execution/strategy-execution.module.ts
+++ packages/tse-backend/src/modules/strategy-execution/strategy-execution.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ExecutionWorkerService } from './execution-worker.service';
 import { TradingStrategyModule } from '../trading-strategy/trading-strategy.module';
+import { SchedulerUtil } from '../../common/utils/scheduler.utils';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TradingStrategyModule],
-  providers: [ExecutionWorkerService]
+  providers: [ExecutionWorkerService, ConfigService, SchedulerUtil]
 })
 export class StrategyExecutionModule {}
