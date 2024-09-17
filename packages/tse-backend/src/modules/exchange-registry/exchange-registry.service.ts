8208ecfdc5bb2dd867a9fbef1b0cbc6075a885b0
Michał Walczak <michal.walczak@blockydevs.com>
2024-09-17 12:38:31 +0200

    Remove unused InternalServerErrorException import

--- packages/tse-backend/src/modules/exchange-registry/exchange-registry.service.ts
+++ packages/tse-backend/src/modules/exchange-registry/exchange-registry.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CustomLogger } from '../logger/logger.service';
 import { CcxtGateway } from '../../integrations/ccxt.gateway';
