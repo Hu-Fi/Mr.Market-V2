4a51c0d6dfe3a8a92611f43e8be74d4935798ecc
Michał Walczak <michal.walczak@blockydevs.com>
2024-09-20 14:30:56 +0200

    Add UserBalanceModule to Transaction module
    This change ensures that the UserBalanceModule is imported in the Transaction module. This integration allows transaction functionalities to access and update user balance information, enhancing the transaction service's capability.

--- packages/mm-backend/src/modules/transaction/transaction.module.ts
+++ packages/mm-backend/src/modules/transaction/transaction.module.ts
@@ -6,11 +6,13 @@ import { Transaction } from '../../common/entities/transaction.entity';
 import { TransactionRepository } from './transaction.repository';
 import { IntegrationsModule } from '../../integrations/integrations.module';
 import { TransactionProfile } from './transaction.mapper';
+import { UserBalanceModule } from '../user-balance/user-balance.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Transaction]),
     IntegrationsModule,
+    UserBalanceModule,
   ],
   providers: [TransactionService, TransactionRepository, TransactionProfile],
   controllers: [TransactionController],
