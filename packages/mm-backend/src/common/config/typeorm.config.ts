e41541edae7bb4a078723e4a6b79d414740944e7
Michał Walczak <michal.walczak@blockydevs.com>
2024-09-20 14:23:10 +0200

    Add migration file for user_balance table

--- packages/mm-backend/src/common/config/typeorm.config.ts
+++ packages/mm-backend/src/common/config/typeorm.config.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DataSourceOptions, LoggerOptions } from 'typeorm';
 import { User } from '../entities/user.entity';
+import { UserBalance } from '../entities/user-balance.entity';
 
 @Injectable()
 export class TypeormConfig {
@@ -27,7 +28,7 @@ export class TypeormConfig {
       username: this.configService.get<string>('DATABASE_USERNAME', 'postgres'),
       password: this.configService.get<string>('DATABASE_PASSWORD', 'postgres'),
       database: this.configService.get<string>('DATABASE_NAME', 'mr_market_v2'),
-      entities: [User],
+      entities: [User, UserBalance],
       migrations: [__dirname + '/../../../migrations/*{.ts,.js}'],
       logging: logging,
       synchronize:
