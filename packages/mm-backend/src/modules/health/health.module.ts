c107862a78eea0293ac14efa06266ef754155440
Michał Walczak <michal.walczak@blockydevs.com>
2024-10-01 15:55:34 +0200

    Add TseHealthService to monitor trading strategy execution

--- packages/mm-backend/src/modules/health/health.module.ts
+++ packages/mm-backend/src/modules/health/health.module.ts
@@ -4,13 +4,16 @@ import { HealthController } from './health.controller';
 import { IntegrationsModule } from '../../integrations/integrations.module';
 import { ConfigService } from '@nestjs/config';
 import { DbHealthService } from './db.health.service';
+import { TseHealthService } from './tse.health.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [IntegrationsModule],
+  imports: [IntegrationsModule, HttpModule],
   providers: [
     ConfigService,
     HealthService,
     DbHealthService,
+    TseHealthService,
   ],
   controllers: [HealthController],
 })
