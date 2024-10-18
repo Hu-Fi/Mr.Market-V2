4acd449778f4c46255c7c7769ac87f9108474d76
Michał Walczak <michal.walczak@blockydevs.com>
2024-10-18 15:42:11 +0200

    Add support for test containers and e2e improvements

--- packages/mm-backend/test/app.e2e-spec.ts
+++ packages/mm-backend/test/app.e2e-spec.ts
@@ -1,20 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
 import * as dotenv from 'dotenv';
 import axios from 'axios';
+import { PostgreSqlContainer, StartedPostgreSqlContainer } from '@testcontainers/postgresql';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisContainer, StartedRedisContainer } from '@testcontainers/redis';
+import { AppModule } from '../src/app.module';
+import { Withdraw } from '../src/common/entities/withdraw.entity';
+import { Deposit } from '../src/common/entities/deposit.entity';
+import { DataSource } from 'typeorm';
 
 dotenv.config();
 
+jest.mock('typeorm-transactional', () => ({
+  Transactional: () =>
+    jest.fn((_target: any, _key: string, descriptor: PropertyDescriptor) => {
+      return descriptor;
+    }),
+  initializeTransactionalContext: jest.fn(() => true),
+  addTransactionalDataSource: jest.fn((dataSource) => dataSource),
+  deleteDataSourceByName: jest.fn(),
+}));
+
 describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
+  let postgresContainer: StartedPostgreSqlContainer;
+  let redisContainer: StartedRedisContainer;
 // api keys
   const CAMPAIGN_LAUNCHER_API_KEY = process.env.E2E_CAMPAIGN_LAUNCHER_API_KEY;
   const RECORDING_ORACLE_API_KEY = process.env.E2E_RECORDING_ORACLE_API_KEY;
   const USER_BEARER = process.env.E2E_USER_BEARER;
+  const MM_USER_BEARER = process.env.E2E_MRMARKET_USER_BEARER;
 
 // apis
   const CAMPAIGN_LAUNCHER_API = process.env.E2E_CAMPAIGN_LAUNCHER_API;
   const RECORDING_ORACLE_API = process.env.E2E_RECORDING_ORACLE_API;
+  const TSE_APP_API = process.env.E2E_TSE_APP_API;
 
 // endpoints
   const UPLOAD_MANIFEST_ENDPOINT = '/manifest/upload';
@@ -23,6 +47,7 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
   const REGISTER_BOT_TO_CAMPAIGN_ENDPOINT = '/mr-market/campaign';
   const CHECK_IF_REGISTERED_TO_CAMPAIGN_ENDPOINT = '/user/campaign';
   const CALCULATE_LIQUIDITY_SCORE_ENDPOINT = '/liquidity-score/calculate';
+  const CREATE_ARBITRAGE_ENDPOINT = '/arbitrage/create-arbitrage';
 
 // constants
   const TRUSTED_ADDRESS = process.env.E2E_TRUSTED_ADDRESS;
@@ -37,15 +62,47 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
   let TESTED_CAMPAIGN: string;
 
   beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [],
+    postgresContainer = await new PostgreSqlContainer()
+      .withName('testcontainer')
+      .withDatabase('testcontainer')
+      .start();
+    redisContainer = await new RedisContainer()
+      .withExposedPorts(6379)
+      .start();
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [
+        AppModule,
+        ConfigModule.forRoot({
+          isGlobal: true,
+          envFilePath: '.env',
+        }),
+        TypeOrmModule.forRootAsync({
+          useFactory: () => {
+            return {
+              type: 'postgres',
+              host: postgresContainer.getHost(),
+              port: postgresContainer.getPort(),
+              username: postgresContainer.getUsername(),
+              password: postgresContainer.getPassword(),
+              database: postgresContainer.getDatabase(),
+              entities: [Deposit, Withdraw],
+              synchronize: true,
+            };
+          },
+        }),
+        TypeOrmModule.forFeature([Deposit, Withdraw]),
+      ],
     }).compile();
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    dataSource = moduleRef.get<DataSource>(DataSource);
     await app.init();
-  });
+  }, 60000);
 
   afterAll(async () => {
+    await dataSource.destroy();
     await app.close();
+    await postgresContainer.stop();
+    await redisContainer.stop();
   });
 
   it.skip('1a. should the user create a new campaign', async () => {
@@ -80,9 +137,9 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
 
   it('1b. should the user join an existing campaign', async () => {
     const campaigns = await fetchCampaignsByChainId(Number(CHAIN_ID));
-    const filteredByChainId = campaigns.filter(campaign => campaign.chainId == CHAIN_ID);
+    const filteredByChainId = campaigns.filter((campaign: { chainId: number; }) => campaign.chainId == CHAIN_ID);
 
-    const foundCampaign = await filteredByChainId.find(async campaign =>
+    const foundCampaign = await filteredByChainId.find(async (campaign: { id: string; }) =>
       Boolean(await checkIfUserIsRegisteredToTheCampaign(campaign.id))
     );
 
@@ -131,7 +188,7 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
     }
   });
 
-  it('3. should the bot fetch available campaigns', async () => {
+  it.only('3. should the bot fetch available campaigns', async () => {
     // MrMarket V2 does not implement this functionality at the moment. Therefore, only the request to the external service is checked, as if the bot were doing it.
     const campaigns = await fetchCampaignsByChainId(Number(CHAIN_ID));
 
@@ -175,11 +232,33 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
   });
 
   it('6b. should the user be able to create their own strategy', async () => {
+    const payload = {
+      userId: '123',
+      clientId: '456',
+      pair: 'ETH/USDT',
+      amountToTrade: 1.0,
+      minProfitability: 0.01,
+      exchangeAName: 'binance',
+      exchangeBName: 'mexc',
+      checkIntervalSeconds: 10
+    };
 
+    const response = await createStrategyByUser(payload);
+    expect(response.status).toEqual(201);
   });
 
   it('7. should the user deposit funds into the bot wallet to increase the liquidity of the campaign', async () => {
+    const dto = {
+      amount: 1000,
+      assetId: '43d61dcd-e413-450d-80b8-101d5e903357',
+      chainId: '43d61dcd-e413-450d-80b8-101d5e903357',
+    };
 
+    await request(app.getHttpServer())
+      .post('/transaction/deposit')
+      .set('Authorization', `Bearer ${MM_USER_BEARER}`)
+      .send(dto)
+      .expect(201);
   });
 
   it.skip('8. should rewards be distributed at the end of the campaign', async () => {
@@ -250,6 +329,20 @@ describe('Exchange oracle (mr market) integration with Hu-Fi (e2e)', () => {
         headers: {
           'Content-Type': 'application/json',
           'x-api-key': RECORDING_ORACLE_API_KEY
+        },
+      }
+    );
+  }
+
+  async function createStrategyByUser(
+    payload: any,
+  ) {
+    return await axios.post(
+      `${TSE_APP_API}${CREATE_ARBITRAGE_ENDPOINT}`,
+      payload,
+      {
+        headers: {
+          'Content-Type': 'application/json',
         },
       }
     );
